59e86e671b5e9cda2fd83d7977df3d7ddaf462db
nk-akun <nk_akun@163.com>
2020-10-01 19:54:17 +0800

    Add sql table user

---
+++ tables.sql
@@ -0,0 +1,22 @@
+CREATE TABLE user_info (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    username VARCHAR(20) UNIQUE NOT NULL,
+    nick_name VARCHAR(30) NOT NULL,
+    password VARCHAR(30),
+    avatar_url VARCHAR(200) NOT NULL,
+    gender TINYINT NOT NULL,
+    email VARCHAR(50),
+    email_verified TINYINT NOT NULL DEFAULT 0,
+    description varchar(200),
+    attention_count INT NOT NULL DEFAULT 0,
+    favourite_article_count INT NOT NULL DEFAULT 0,
+    fans_count INT NOT NULL DEFAULT 0,
+    post_count INT NOT NULL DEFAULT 0,
+    comment_count INT NOT NULL DEFAULT 0,
+    type INT NOT NULL DEFAULT 0,
+    city VARCHAR(40),
+    province VARCHAR(40),
+    country VARCHAR(40),
+    create_time BIGINT,
+    update_time BIGINT
+);
